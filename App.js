266c647bac1d70b59b6f4033afde9c1b8876ee42
frank06n <dev.code38@gmail.com>
2024-09-24 11:16:33 +0530

    small fix App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AllChatsScreen from './screens/AllChatsScreen';
-import ChatScreen from './screens/ChatScreen';
+// import ChatScreen from './screens/ChatScreen';
 import { Provider as PaperProvider } from 'react-native-paper';
 
 const Stack = createStackNavigator();
@@ -13,7 +13,7 @@ export default function App() {
             <NavigationContainer>
                 <Stack.Navigator initialRouteName="AllChats">
                     <Stack.Screen name="AllChats" component={AllChatsScreen} options={{ title: 'Chats' }} />
-                    <Stack.Screen name="Chat" component={ChatScreen} options={({ route }) => ({ title: route.params.name })} />
+                    {/* <Stack.Screen name="Chat" component={ChatScreen} options={({ route }) => ({ title: route.params.name })} /> */}
                 </Stack.Navigator>
             </NavigationContainer>
         </PaperProvider>
